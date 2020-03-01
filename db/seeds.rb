1da7d4858573ea847224153b2e0f2aea0724d385
atone3 <atone3@illinois.edu>
2020-03-01 03:32:23 +0000

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+CSV.foreach(Rails.root.join('lib/Austin_Animal_Center_Outcomes.csv'), headers: true) do |row|
+  
+  Animal.create({
+    name: row[0],
+    birthday: row[1],
+    outcometype: row[2],
+    outcomesubtype: row[3],
+    animaltype: row[4],
+    sex: row[5],
+    breed: row[6],
+    color: row[7]
+  })
+end
