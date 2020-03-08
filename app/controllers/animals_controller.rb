66051a605ce1173249a0135ffbedf50bd0555502
atone3 <atone3@illinois.edu>
2020-03-08 16:27:02 +0000

    ordering records by id for stage 3 submission

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -4,7 +4,7 @@ class AnimalsController < ApplicationController
   # GET /animals
   # GET /animals.json
   def index
-    @animals = Animal.all.paginate(:page => params[:page], :per_page => 1000)
+    @animals = Animal.all.order(:id).paginate(:page => params[:page], :per_page => 1000)
   end
 
   # GET /animals/1
