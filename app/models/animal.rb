81a1071c29947d3a9080906ca9584adeb9174367
atone3 <atone3@illinois.edu>
2020-04-11 16:39:41 +0000

    added validations to animal model, form validation styling

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,4 @@
 class Animal < ApplicationRecord
+  
+  validates :name, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,6 @@ CSV.foreach(Rails.root.join('lib/Austin_Animal_Center_Outcomes.csv'), headers: t
   name = row[0]
   birthday = row[1]
   
-  a = Animal.find_or_initialize_by(name: name, birthday: birthday)
-  
   if (name.blank? || name.nil? || name.include?("0") || name.include?("1") || name.include?("2") || name.include?("3") || name.include?("4") || name.include?("5") || name.include?("6") || name.include?("7") || name.include?("8") || name.include?("9"))
     if (row[4].downcase == 'cat')
       name = Faker::Creature::Cat.name
@@ -24,11 +22,11 @@ CSV.foreach(Rails.root.join('lib/Austin_Animal_Center_Outcomes.csv'), headers: t
     elsif (row[5].downcase.include? "female")
       name = Faker::Name.female_first_name
     else 
-      Faker::Name.male_first_name 
+      name = Faker::Name.male_first_name 
     end
   end
   
-  a.update_attributes(
+  Animal.create(
     :name =>  name,
     :birthday => row[1],
     :outcometype => row[2],
