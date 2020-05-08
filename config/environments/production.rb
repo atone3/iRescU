b92a8e016c45f1001b215a7ffc048dbea62aa412
atone3 <atone3@illinois.edu>
2020-05-08 21:39:39 +0000

    updated s3 access:

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,10 +95,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: 'irescu-bucket',
-    access_key_id: 'AKIAJEJALPXKZRLETMAQ',
-    secret_access_key: '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',
-    s3_region: 'us-east-1',
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
   }
 }
 end
